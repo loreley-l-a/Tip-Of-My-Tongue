79dbdcf1331b188e210743e2e5e0bedb26f58f48
loreley <loreleynlopezarias@gmail.com>
2026-08-31 15:06:32 -0400

    added starter code to main.,py
    investigating NLP mpdel...

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from sentence_transformers import SentenceTransformer, util
 
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+secret_word = "fruit"
+user_guess = "apple"
+
+embeddings = model.encode([secret_word, user_guess])
+similarity = util.cos_sim(embeddings[0], embeddings[1])
+
+print(f"Similarity score: {similarity.item():.4f}")
